51376aa7dc5918f16f13dc9313e3f6d35fafc084
Randy Abernethy <randy.abernethy@rx-m.com>
2017-10-10 17:37:56 -0700

    added hostname and ip to service output

--- src/hi.go
+++ src/hi.go
@@ -1,12 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 func main() {
-	http.Handle("/", http.HandlerFunc(hi))
-	http.ListenAndServe("localhost:9999", nil)
+	//Set listening port
+	port := 9898
+	if len(os.Args) > 1 {
+		var err error
+		port, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	//Find default output IP and hostname
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Register handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(hostname + " " + localAddr.IP.String() + "\n"))
+	})
+	//Serve
+	log.Fatal(http.ListenAndServe("localhost:"+strconv.Itoa(port), nil))
 }
 
-func hi(resp http.ResponseWriter, req *http.Request) {
-	resp.Write([]byte("Hello World!\n"))
-}
